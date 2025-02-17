8dcb524c7742f72349eb8070aa1a9e79a5ae8a86
Alexc <fabiancuaran114@gmail.com>
2025-02-17 11:19:46 -0500

    este archivo tien una operacion de operaciones matematicas

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+print("CALCULADORA BASICA")
+num1=20
+num2=2
+suma=num1+num2
+resta=num1-num2
+division=num1/num2
+multiplicacion=num1*num2
+print("la suma de los dos numeros es", suma)
+print("la resta de los dos numeros es", suma)
+print("la division de los dos numeros es", suma)
+print("la multiplicacion de los dos numeros es", suma)
